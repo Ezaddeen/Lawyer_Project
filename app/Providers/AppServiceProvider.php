9e1e8f05ae30fd9385209b06767753d87933d00a
ezaddeen <ezaddeenaltahesh@gmail.com>
2025-11-28 00:36:12 +0300

    FORCE: Force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,13 +8,14 @@ use Illuminate\Support\Facades\Route;
 use App\Observers\ActivityObserver;
 use App\Models\Document;
 use App\Models\User;
+use Illuminate\Support\Facades\URL; // <--- السطر الأول الذي أضفناه
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register( ): void
     {
         //
     }
@@ -30,5 +31,10 @@ class AppServiceProvider extends ServiceProvider
             Document::observe(ActivityObserver::class);
             User::observe(ActivityObserver::class);
 
+            // --- الكود الجديد الذي أضفناه ---
+            if ($this->app->environment('production')) {
+                URL::forceScheme('https' );
+            }
+            // ---------------------------------
     }
 }
